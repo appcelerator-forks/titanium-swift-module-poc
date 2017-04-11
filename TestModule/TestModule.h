864b24ec6f93f865ef045676cb3c612b37ae7f8a
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-04-11 11:44:02 +0200

    Initial commit

---
+++ TestModule/TestModule.h
@@ -0,0 +1,19 @@
+//
+//  TestModule.h
+//  TestModule
+//
+//  Created by Hans Knöchel on 11.04.17.
+//  Copyright © 2017 Hans Knoechel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestModule.
+FOUNDATION_EXPORT double TestModuleVersionNumber;
+
+//! Project version string for TestModule.
+FOUNDATION_EXPORT const unsigned char TestModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestModule/PublicHeader.h>
+
+
---
+++ TestModule/TestModuleModule.swift
@@ -0,0 +1,14 @@
+//
+//  TestModuleModule.swift
+//  TestModule
+//
+//  Created by Hans Knöchel on 11.04.17.
+//  Copyright © 2017 Hans Knoechel. All rights reserved.
+//
+
+import UIKit
+import SwiftTitanium
+
+class TestModuleModule: TiModule {
+
+}
